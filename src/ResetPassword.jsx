f6602e2eae3345a968c2cffcd0036c4b74227f02
ikanengKhoza <89599256+ikanengKhoza@users.noreply.github.com>
2025-10-30 13:37:39 +0000

    Create ResetPassword.jsx

---
+++ src/ResetPassword.jsx
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Opening App...</title>
+  <script>
+    const hash = window.location.hash;
+    window.location.href = `debtfreedisciple://(auth)/ResetPassword${hash}`;
+  </script>
+</head>
+<body>
+  <p>Opening Debt Free Disciple app...</p>
+</body>
+</html>
